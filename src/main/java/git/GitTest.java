e3269ce48243dd3464495f698cfa8d7baa29c905
Eric <Eric@guigu.com>
2021-06-30 11:05:10 +0800

    hot-fix commit 1

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git 1");
     }
+    public void hotFixPrint(){
+        System.out.println("hot-fix写的第一段代码");
+    }
 }
