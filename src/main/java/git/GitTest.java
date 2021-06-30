1ed61372c9bc857583f04200e6fdd8678de1f135
Eric <Eric@guigu.com>
2021-06-30 11:06:19 +0800

    master commit 1

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git 1");
     }
+    public void masterPrint(){
+        System.out.println("master写的第一段代码");
+    }
 }
