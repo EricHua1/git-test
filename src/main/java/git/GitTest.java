df8f896509f05c7f9b11702527ed30d69e5a0178
Eric <Eric@guigu.com>
2021-06-30 11:12:43 +0800

    master commit 2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -3,11 +3,14 @@ package git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git 1");
+        masterPrint();
+        hotFixPrint();
+
     }
-    public void masterPrint(){
+    public static void masterPrint(){
         System.out.println("master写的第一段代码");
     }
-    public void hotFixPrint(){
+    public static void hotFixPrint(){
         System.out.println("hot-fix写的第一段代码");
     }
 }
